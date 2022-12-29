1a655023239188a965e799b7841a004aeaebc4d6
shubham kshirsagar <117492782+Shubhamkshirsagar1@users.noreply.github.com>
2022-12-29 13:37:25 +0530

    Done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+
+const textbox = document.getElementById("evaluatedText");
+const wordCount = document.getElementById("wordCount");
+
+
+textbox.addEventListener("input", function() {
+
+  const count = this.value.split(" ").length;
+
+  wordCount.innerHTML = count;
+});
